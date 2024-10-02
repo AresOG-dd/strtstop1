de0f947e250f11a43c1c6c7022d46f665603aa52
AresOG-dd <navpreet1057@gmail.com>
2024-10-02 17:31:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/titan/titan.py",
+    "postStartCommand": "python3 /workspaces/Titan/titan.py",
     "customizations": {
         "vscode": {
             "settings": {
